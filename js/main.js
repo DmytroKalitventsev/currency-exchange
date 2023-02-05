558b79aa90e33925326d63bc6435ac4e0d65b594
Dmytro <dm.kalitventsev@gmail.com>
2023-02-05 20:37:57 +0200

    displayed the exchange rate in the console

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//TODO Потрібно вивести в консоль наступне:
+//TODOСумма від користувача = курсу кожної з валют на ринку
+
+// Отримана сума від користувача
+let sum = +prompt('Введіть в гривнях суму для обміну');
+
+// Курс валют на ринку
+let usd = 40.30;
+let eur = 43.75;
+let pln = 9.20;
+let gpb = 49.20;
+let chf = 43.50;
+
+// Конвертація отриманої суми від користувача в курс валют на ринку
+let resUsd = sum / usd;
+let resEur = sum / eur;
+let resPln = sum / pln;
+let resGpb = sum / gpb;
+let resChf = sum / chf;
+
+// Виведення результату користувачу
+console.log(sum + '(грн)' + ' = ' + +resUsd.toFixed(2) + '($)');
+console.log(sum + '(грн)' + ' = ' + +resEur.toFixed(2) + '(€)');
+console.log(sum + '(грн)' + ' = ' + +resPln.toFixed(2) + '(zł)');
+console.log(sum + '(грн)' + ' = ' + +resGpb.toFixed(2) + '(£)');
+console.log(sum + '(грн)' + ' = ' + +resChf.toFixed(2) + '(Fr)');
